6c232df168ddd633436e48cb20bc081071626135
chiacheng59 <56727237+chiacheng59@users.noreply.github.com>
2024-07-11 13:04:26 +0800

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Travel",
+  description: "Travel UI/UX App for Camping",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
